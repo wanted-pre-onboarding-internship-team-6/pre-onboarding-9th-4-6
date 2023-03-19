f32b8b1fc0832502e60c744cd962b9a38e9092b2
Chun-gu <chungu.yi92@gmail.com>
2023-03-20 02:11:00 +0900

    [#1] feat: useQuery로 주문목록 데이터 불러와서 테이블 컴포넌트로 출력

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getOrders } from '@/api';
+import { OrderTable } from '@/components';
+
 export default function App() {
-  return <div></div>;
+  const {
+    isLoading,
+    isError,
+    data: orders,
+  } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders,
+    select: (data) =>
+      data.filter(
+        (order) => order.transaction_time.split(' ')[0] === '2023-03-08',
+      ),
+    refetchInterval: 1000 * 5,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
+
+  return <OrderTable orders={orders} />;
 }
