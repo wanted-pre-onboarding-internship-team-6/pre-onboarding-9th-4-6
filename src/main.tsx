e1a976e0012b37358f457819748cfdf989a6dad5
Chun-gu <chungu.yi92@gmail.com>
2023-03-20 20:58:02 +0900

    [#1] feat: Router 작성 및 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,14 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import App from './App';
+import Router from './Router';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Router />
       <ReactQueryDevtools initialIsOpen={false} position={'bottom-right'} />
     </QueryClientProvider>
   </React.StrictMode>,
