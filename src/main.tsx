71433f2a17b89e7bbba316ede2002d2574ad8f6d
Chun-gu <chungu.yi92@gmail.com>
2023-03-20 17:38:01 +0900

    [#1] refactor: devtools 위치 조정

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false} position={'bottom-right'} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
