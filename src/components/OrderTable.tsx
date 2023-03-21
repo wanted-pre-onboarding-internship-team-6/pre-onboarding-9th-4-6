cd03742571b4c1168c5cf6fe600fc7b7e1b54b45
Chun-gu <chungu.yi92@gmail.com>
2023-03-21 21:42:17 +0900

    [#11] style: Layout 컴포넌트 구현 및 스타일링

--- src/components/OrderTable.tsx
+++ src/components/OrderTable.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useEffect, useRef } from 'react';
 
+import styled from '@emotion/styled';
 import { useSearchParams } from 'react-router-dom';
 
 import {
@@ -59,7 +60,7 @@ export default function OrderTable({ orderData }: Props) {
 
   const debouncedSearchCustomer = debounce(searchCustomer, 400);
 
-  const sortIndicator = order === SORT_ORDER.asc ? '오름차순' : '내림차순';
+  const sortIndicator = order === SORT_ORDER.asc ? '▲' : '▼';
 
   useEffect(() => {
     if (customerSearchRef.current && keyword) customerSearchRef.current.focus();
@@ -67,7 +68,7 @@ export default function OrderTable({ orderData }: Props) {
 
   return (
     <>
-      <div>
+      <SearchBar>
         총 주문: {totalOrder}건
         <input
           type="text"
@@ -76,15 +77,15 @@ export default function OrderTable({ orderData }: Props) {
           placeholder="고객 이름 검색"
           onChange={debouncedSearchCustomer}
         />
-      </div>
-      <table>
-        <thead>
-          <tr>
+      </SearchBar>
+      <Table>
+        <Thead>
+          <Tr>
             <th onClick={() => sortOrders(ORDER_KEY.id)}>
-              주문번호{sort === ORDER_KEY.id && sortIndicator}
+              주문번호 {sort === ORDER_KEY.id && sortIndicator}
             </th>
             <th onClick={() => sortOrders(ORDER_KEY.transactionTime)}>
-              거래시간{sort === ORDER_KEY.transactionTime && sortIndicator}
+              거래시간 {sort === ORDER_KEY.transactionTime && sortIndicator}
             </th>
             <th>
               주문처리상태
@@ -97,22 +98,22 @@ export default function OrderTable({ orderData }: Props) {
             <th>고객번호</th>
             <th>고객이름</th>
             <th>가격</th>
-          </tr>
-        </thead>
-        <tbody>
+          </Tr>
+        </Thead>
+        <Tbody>
           {orders.map((order) => (
-            <tr key={order.id}>
+            <Tr key={order.id}>
               <td>{order.id}</td>
               <td>{order.transaction_time}</td>
               <td>{order.status ? 'O' : 'X'}</td>
               <td>{order.customer_id}</td>
               <td>{order.customer_name}</td>
               <td>{order.currency}</td>
-            </tr>
+            </Tr>
           ))}
-        </tbody>
-      </table>
-      <div>
+        </Tbody>
+      </Table>
+      <Pagination>
         {Array.from({ length: totalPageCount }).map((_, i) => (
           <button
             key={i}
@@ -121,7 +122,63 @@ export default function OrderTable({ orderData }: Props) {
             {i + 1}
           </button>
         ))}
-      </div>
+      </Pagination>
     </>
   );
 }
+
+const SearchBar = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'end',
+  fontSize: '20px',
+  fontWeight: 'bold',
+  paddingTop: '48px',
+  marginBottom: '20px',
+
+  input: {
+    width: '223px',
+    height: '48px',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    paddingLeft: '11px',
+  },
+});
+
+const Table = styled.table({
+  display: 'block',
+  width: '100%',
+  height: '580px',
+  overflowY: 'scroll',
+  border: '1px solid #000000',
+  marginBottom: '48px',
+});
+
+const Thead = styled.thead({
+  width: '100%',
+});
+
+const Tbody = styled.tbody({});
+
+const Tr = styled.tr({
+  display: 'grid',
+  gridTemplateColumns: '1fr 2fr 1fr 1fr 2fr 1fr',
+  height: '48px',
+
+  td: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+
+  button {
+    width: 48px;
+    height: 48px;
+    margin: 0 12px;
+  }
+`;
