035ff8ccd887aa0d6790ff2ce1c74edff12204d9
pji0219 <pji219@naver.com>
2023-03-22 21:24:00 +0900

    [#18] feat: 테스트 코드 추가
    OderTable 컴포넌트에 대한 테스트 코드를 추가 했습니다.
    
    관련 이슈 #18

---
+++ src/test/OrderTable.test.ts
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+// import OrderTable from '@/components/OrderTable';
+
+test('table rendering', () => {
+  // render(<OrderTable />);
+
+  const orderTh = screen.getByText('주문번호');
+  const transactionTimeTh = screen.getByText('거래시간');
+  const orderStatusTh = screen.getByText('주문처리상태');
+  const searchBarInput = screen.getByPlaceholderText('고객 이름 검색');
+
+  userEvent.click(searchBarInput);
+  userEvent.type(searchBarInput, '이름');
+
+  userEvent.click(orderTh);
+  userEvent.click(transactionTimeTh);
+  userEvent.click(orderStatusTh);
+
+  expect(searchBarInput).toHaveFocus();
+  expect(searchBarInput).toBeInTheDocument();
+
+  expect(orderStatusTh).toBeTruthy();
+  expect(orderStatusTh).toBeFalsy();
+});
