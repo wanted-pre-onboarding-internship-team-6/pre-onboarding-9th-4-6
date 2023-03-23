592096276b891cdc413aee8f00d53219c0ad6127
dong-geon-Lee <ch9716@naver.com>
2023-03-23 17:45:30 +0900

    [#22] test: jest와 리액트 라이브러리를 이용해서 테스트 코드 작성

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -6,10 +6,12 @@ import { describe, it } from 'vitest';
 
 import { OrderTable } from '@/components';
 
+import mockDatas from '../../public/mock_data.json';
+
 const queryClient = new QueryClient();
 
 describe(`<OrderTable />`, () => {
-  it('matches snapshot', () => {
+  it('OrderTable 컴포넌트 렌더링 상태 검사', () => {
     const utils = render(
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
@@ -22,7 +24,7 @@ describe(`<OrderTable />`, () => {
   });
 });
 
-describe(`<OrderTable />`, () => {
+describe(`OrderTable 컴포넌트 select 요소 렌더링 상태 검사`, () => {
   it('select match', () => {
     const utils = render(
       <select data-testid="select">
@@ -35,7 +37,7 @@ describe(`<OrderTable />`, () => {
   });
 });
 
-describe('Pagination component', () => {
+describe('페이지네이션 버튼 작동 상태 검사', () => {
   const totalPageCount = 5;
   const setPage = vi.fn();
   const page = '3';
@@ -56,18 +58,17 @@ describe('Pagination component', () => {
     );
   });
 
-  it('should render correct number of buttons', () => {
+  it('버튼 개수가 페이지 개수와 일치하는지 검사', () => {
     const buttons = screen.getAllByTestId('my-button');
-
     expect(buttons).toHaveLength(totalPageCount);
   });
 
-  it('should disable the current page button', () => {
+  it('현재 페이지를 나타내는 버튼이 비활성화 되는지 검사', () => {
     const activeButton = screen.getByText(page);
     expect(activeButton).toBeDisabled();
   });
 
-  it('should call setPage when a button is clicked', () => {
+  it('사용자가 5번 페이지 버튼을 눌렀을 떄, 페이지가 5번으로 이동하는 함수가 작동하는지 검사', () => {
     const buttons = screen.getAllByTestId('my-button');
 
     const targetPage = 5;
@@ -76,4 +77,45 @@ describe('Pagination component', () => {
     expect(setPage).toHaveBeenCalledTimes(1);
     expect(setPage).toHaveBeenCalledWith(targetPage);
   });
+});
+
+test('mockDatas의 데이터들이 테이블 요소에 렌더링 되는지 검사', () => {
+  const utils = render(
+    <tbody>
+      {mockDatas.map((order) => (
+        <tr key={order.id}>
+          <td>{order.id}</td>
+          <td>{order.transaction_time}</td>
+          <td>{order.status ? 'O' : 'X'}</td>
+          <td>{order.customer_id}</td>
+          <td>{order.customer_name}</td>
+          <td>{order.currency}</td>
+        </tr>
+      ))}
+    </tbody>,
+  );
+  expect(utils.container).toMatchSnapshot();
+});
+
+test('주문목록 데이터 객체의 타입 검사', () => {
+  expect(mockDatas).toContainEqual(
+    expect.objectContaining({
+      id: expect.any(Number),
+      transaction_time: expect.any(String),
+      status: expect.any(Boolean),
+      customer_id: expect.any(Number),
+      customer_name: expect.any(String),
+      currency: expect.any(String),
+    }),
+  );
+});
+
+test('mockDatas의 transaction_time에 2023-03-08이 포함 된 데이터 수 검사', () => {
+  const matchingData = mockDatas.filter((data) =>
+    /2023-03-08/.test(data.transaction_time),
+  );
+
+  const count = matchingData.length;
+  expect(count).toBeGreaterThan(0);
+  expect(count).toBe(267);
 });
