321e7b37fc39ffbf69bdbbc07a5bc4a21156a770
Park Su Been <71866185+bymine@users.noreply.github.com>
2023-03-23 16:28:24 +0900

    [#22] test: Pagination Button 테스트 코드 작성 (#25)
    버튼 생성, 버튼 Disabled, 버튼 클릭에 대한 테스트 코드 작성

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-
 import '@testing-library/jest-dom';
+import { describe, it } from 'vitest';
 
 import { OrderTable } from '@/components';
 
@@ -32,5 +32,48 @@ describe(`<OrderTable />`, () => {
       </select>,
     );
     expect(utils.container).toMatchSnapshot();
+  });
+});
+
+describe('Pagination component', () => {
+  const totalPageCount = 5;
+  const setPage = vi.fn();
+  const page = '3';
+
+  beforeEach(() => {
+    render(
+      <>
+        {Array.from({ length: totalPageCount }).map((_, i) => (
+          <button
+            data-testid="my-button"
+            key={i}
+            disabled={page === String(i + 1)}
+            onClick={() => setPage(i + 1)}>
+            {i + 1}
+          </button>
+        ))}
+      </>,
+    );
+  });
+
+  it('should render correct number of buttons', () => {
+    const buttons = screen.getAllByTestId('my-button');
+
+    expect(buttons).toHaveLength(totalPageCount);
+  });
+
+  it('should disable the current page button', () => {
+    const activeButton = screen.getByText(page);
+    expect(activeButton).toBeDisabled();
+  });
+
+  it('should call setPage when a button is clicked', () => {
+    const buttons = screen.getAllByTestId('my-button');
+
+    const targetPage = 5;
+    fireEvent.click(buttons[4]);
+
+    expect(setPage).toHaveBeenCalledTimes(1);
+    expect(setPage).toHaveBeenCalledWith(targetPage);
   });
 });
