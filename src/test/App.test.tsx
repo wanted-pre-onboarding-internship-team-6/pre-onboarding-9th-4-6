3dd8060df8a0dd1ec763760674b43391630f9150
pji0219 <pji219@naver.com>
2023-03-23 16:04:11 +0900

    [#22] test: 테스트코드 보충 작성
    테스트 코드를 보충 작성하였습니다.
    
    관련 이슈 #22

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
-
 import '@testing-library/jest-dom';
 
 import { OrderTable } from '@/components';
@@ -17,6 +17,22 @@ describe(`<OrderTable />`, () => {
         </BrowserRouter>
       </QueryClientProvider>,
     );
+
+    const orderTh = screen.getByText('주문번호');
+    const transactionTimeTh = screen.getByText('거래시간');
+    const orderStatusTh = screen.getByText('주문처리상태');
+    const searchBarInput = screen.getByPlaceholderText('고객 이름 검색');
+
+    expect(orderTh).toBeInTheDocument();
+    expect(transactionTimeTh).toBeInTheDocument();
+    expect(orderStatusTh).toBeInTheDocument();
+    expect(searchBarInput).toBeInTheDocument();
+
+    userEvent.click(searchBarInput);
+    expect(searchBarInput).toHaveFocus();
+
+    userEvent.type(searchBarInput, '이름');
+    expect(searchBarInput).toBeInTheDocument();
 
     expect(utils.container).toMatchSnapshot();
   });
