aa1544b9f711d76c6d00d45ec29c64e21fadb5ec
Chun-gu <chungu.yi92@gmail.com>
2023-03-23 15:08:45 +0900

    [#22] refactor: useOrders 훅에서 useQueryString 사용하도록 변경하고 isLoading, isError 없애고 suspense 적용

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -1,20 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
-import { useSearchParams } from 'react-router-dom';
 
 import { getOrders } from '@/api';
 import { QUERY_KEY } from '@/constants';
 
-export default function useOrders() {
-  const [searchParams] = useSearchParams();
-  const { page, sort, order, isDone, keyword } = Object.fromEntries([
-    ...searchParams,
-  ]);
+import useQueryString from './useQueryString';
 
-  const {
-    isLoading,
-    isError,
-    data: orderData,
-  } = useQuery({
+export default function useOrders() {
+  const { getQueryString } = useQueryString();
+  const { page, sort, order, isDone, keyword } = getQueryString();
+
+  const { data } = useQuery({
     queryKey: [QUERY_KEY.orders, { page, sort, order, isDone, keyword }],
     queryFn: () =>
       getOrders({
@@ -26,7 +21,11 @@ export default function useOrders() {
       }),
     staleTime: 1000 * 5,
     refetchInterval: 1000 * 5,
+    suspense: true,
+    retry: false,
   });
 
-  return { isLoading, orderData, isError };
+  const orderData = data ? data : { totalOrder: 0, orders: [] };
+
+  return { orderData };
 }
